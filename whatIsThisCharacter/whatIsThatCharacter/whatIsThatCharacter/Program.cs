2ae579543cba3145809150d1e974f5ecd789ca0f
3llioth <130607739+OZALOMARLI@users.noreply.github.com>
2025-08-03 18:29:58 +0300

    Add files via upload

---
+++ whatIsThisCharacter/whatIsThatCharacter/whatIsThatCharacter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace whatIsThisCharacter;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var k = (char)Console.Read();
+        if (char.IsDigit(k))
+        {
+            Console.WriteLine("It's a number! ");
+        }
+        else if (char.IsLower(k))
+        {
+            Console.WriteLine("It's Lowercase! ");
+        }
+        else if (char.IsUpper(k))
+        {
+            Console.WriteLine("It's Uppercase! ");
+        }
+        else
+        {
+            Console.WriteLine("It's Unknown Character! ");
+        }
+    }
+}
---
+++ whatIsThisCharacter/whatIsThisCharacter.cs
@@ -0,0 +1,27 @@
+usinging System;
+
+namespace BtkAkademiDersleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var k = (char)Console.Read();
+            if (char.IsDigit(k))
+            {
+                Console.WriteLine("It's a number! ")
+            }
+            else if (char.IsLower(k))
+            {
+                Console.WriteLine("It's Lowercase! ")
+            }
+            else if (char.IsUpper(k))
+            { 
+                Console.WriteLine("It's Uppercase! ")
+            }
+            else 
+            {
+                Console.WriteLine ("It's unknown Character! ")
+            }
+        }
+    }
